4a09d4ff9181bd71c689483fd78b8463cdc8aa68
eric cao <caoyu88@gmail.com>
2018-09-01 12:13:17 -0400

    checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { AppModule } from './app.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
